94155eb51be70d28e1e34d090a82e50c849c50d0
Mohammad Aslam <aslammohammad336@gmail.com>
2025-05-16 18:31:18 +0530

    Initial commit

---
+++ homepage/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.http import HttpResponse
+from django.conf import settings
+from datetime import datetime
+
+# Create your views here.
+
+
+def index(request):
+    context ={
+        'platform_name': settings.PLATFORM_NAME,
+        'platform_first_name': settings.PLATFORM_FIRST_NAME,
+        'platform_last_name': settings.PLATFORM_LAST_NAME,
+        'current_date': datetime.now().year
+    }
+    return render(request, 'homepage/index.html', context)
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'homepage/signup.html', {'form': form})
